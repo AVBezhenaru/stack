a3e8917e97c4dfb645b798fbb599f2cdf7e9de8e
Anatoliy Bezhenaru <bejena-ru@ya.ru>
2019-02-25 01:14:47 +0700

    Transferred functions to another file

--- tests.py
+++ tests.py
@@ -1,4 +1,6 @@
 from stack import *
+import operator
+
 
 def sizeTest():
     print("Size test")
@@ -52,3 +54,54 @@ def peekTest():
         print("ERROR")
 
 peekTest()
+
+
+def compareString(str):
+    stack1 = Stack()
+    stack2 = Stack()
+    for i in range(len(str)):
+        if str[i] == "(":
+            stack1.push(str[i])
+        elif str[i] == ")":
+            stack2.push(str[i])
+
+    if stack1.size() == stack2.size():
+        return True
+    else:
+        return False
+
+
+stack = Stack()
+stack.push("=")
+stack.push("+")
+stack.push(9)
+stack.push("*")
+stack.push(5)
+stack.push("+")
+stack.push(2)
+stack.push(8)
+
+
+def postfixСalculation(stack):
+    ops = {'+': operator.add, '-': operator.sub,
+           '*': operator.mul, '/': operator.floordiv}
+
+    stack2 = Stack()
+    for i in range(stack.size()):
+        a = stack.peek()
+        stack.pop()
+
+        if a == "=":
+            result = stack2.peek()
+
+        elif isinstance(a, int):
+            stack2.push(a)
+
+        elif isinstance(a, str):
+            b = stack2.peek()
+            stack2.pop()
+            c = stack2.peek()
+            d = ops[a](b, c)
+            stack2.push(d)
+
+    return result
